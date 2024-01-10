57cd30980a1bcdb31900a4610dff48718dc384ce
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-01-09 18:23:25 -0600

    small changes delete not yet ready

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,45 +1,47 @@
-import React from "react";
-import { useOutletContext } from "react-router-dom";
+import React from 'react';
+import { useOutletContext } from 'react-router-dom';
 const api = 'http://localhost:3000/data';
 
-function Delete(){
-    const { 
-        listNames,
-        setFullNames,
-        searchPerson,
-        setSearchPerson 
-    } = useOutletContext()
+function Delete() {
+  const { listNames, setFullNames, searchPerson, setSearchPerson } =
+    useOutletContext();
 
+  function listeningEventTargetValue(e) {
+    // onChange{ (e)=>setSearchPerson(e.target.value) }
+    setSearchPerson(e.target.value);
+  }
 
-    const handleDelete = (id) => {
-        // Assuming 'id' is the unique identifier for the item to be deleted
-        fetch(`${api}/${id}`, {
-          method: 'DELETE',
-        })
-          .then((res) => res.json())
-          .then((deletedItem) => {
-            // Update the list of names by filtering out the deleted item
-            const updatedList = listNames.filter((item) => item.id !== deletedItem.id);
-            setFullNames(updatedList);
-          })
-          .catch((error) => console.log(error));
-      };
-    
+  const handleDelete = (id) => {
+    // Assuming 'id' is the unique identifier for the item to be deleted
+    fetch(`${api}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((deletedItem) => {
+        // Update the list of names by filtering out the deleted item
+        const updatedList = listNames.filter(
+          (item) => item.id !== deletedItem.id
+        );
+        setFullNames(updatedList);
+      })
+      .catch((error) => console.log(error));
+  };
 
-
-
-
-
-      return(
-        <>
-        <h4>Click on name to delete!</h4>
-            <br/>
-            <br/>
-        <div>
-                <small>{handleDelete}</small>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <h4>Click on name to delete!</h4>
+      <input
+        type="text"
+        placeholder="search person"
+        onChange={listeningEventTargetValue}
+      />
+      <br />
+      <br />
+      <div id="glass">
+        <small>{listNames()}</small>
+      </div>
+    </>
+  );
 }
 
 export default Delete;
